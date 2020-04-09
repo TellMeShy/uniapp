d269fe515cbfef8dace535868c30ffac2b22efa1
zouyang <email@example.com544461472@qq.com>
2020-04-09 18:26:41 +0800

    demo

---
+++ pillReminder/pages/index/index.vue
@@ -0,0 +1,40 @@
+<template>
+	<view class="today page_content">
+		<view class="today_date base_normal_szie">Mar 28, 2020</view>
+		<view class="flex">
+			<view class="title lg_normal_szie">Today</view>
+			    <image class="ico_normal" mode="scaleToFill" src="../../static/image/ioc_add.png"
+			              @error="imageError"></image>
+		</view>
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				title: 'Hello'
+			}
+		},
+		onLoad() {
+
+		},
+		methods: {
+
+		}
+	}
+</script>
+
+<style lang="scss" scoped>
+	.today{
+		background-color: $uni-bg-color-grey;
+		.today_date{
+			padding-top: 29rpx;
+		}
+		.flex{
+			.title{
+				flex: 1;
+			}
+		}
+	}
+</style>
